928245a8958960cd5a32a0285c283a40eedfa31b
khauvukhanh <32231951+khauvukhanh@users.noreply.github.com>
2017-12-30 21:32:50 +0700

    SQLite

--- ContentManager.m
+++ ContentManager.m
@@ -1,15 +1,16 @@
 //
 //  ContentManager.m
-//  Course_CoreData
+//  CourseList
 //
-//  Created by Vu Khanh on 10/28/17.
+//  Created by Vu Khanh on 10/25/17.
 //  Copyright © 2017 Khanh. All rights reserved.
 //
 
 #import "ContentManager.h"
-#import "AppDelegate.h"
+
 
 @implementation ContentManager
+
 + (ContentManager *)shareManager
 {
     static ContentManager *manager = nil;
@@ -21,23 +22,12 @@
     return manager;
 }
 
-- (NSManagedObjectContext *)getCurrentContext
+- (BOOL)checkDatabase
 {
-    AppDelegate *application = (AppDelegate *)[[UIApplication sharedApplication]delegate];
-    return application.persistentContainer.viewContext;
-}
-
-
-
-- (BOOL)insertCourseWithName:(NSString *)name
-{
-    NSManagedObjectContext *context = [self getCurrentContext];
-    Course *course = [NSEntityDescription insertNewObjectForEntityForName:@"Course" inManagedObjectContext:context];
+    NSString *documentPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+    dbPath = [NSString stringWithFormat:@"%@/CourseList.sqlite", documentPath];
     
-    course.courseName = name;
-    
-    NSError *error = nil;
-    if (![context save:&error])
+    if (![[NSFileManager defaultManager] fileExistsAtPath:dbPath])
     {
         return NO;
     }
@@ -47,115 +37,252 @@
     }
 }
 
-- (BOOL)editCourse:(Course *)course
+- (void)copyDatabaseIfNeeded
 {
-    if (course != nil)
+    if (![self checkDatabase])
     {
-        NSManagedObjectContext *context = [self getCurrentContext];
-        NSError *error = nil;
+        [self copyDatabaseToDocument];
+    }
+}
+
+- (void)copyDatabaseToDocument
+{
+    NSError *error = nil;
+    NSString *localPath = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:DB_NAME];
+    
+    /// Remove database if file exists
+    [[NSFileManager defaultManager] removeItemAtPath:dbPath error:&error];
+    
+    if (![[NSFileManager defaultManager] copyItemAtPath:localPath toPath:dbPath error:&error])
+    {
+        NSLog(@"Error - %@", [error localizedDescription]);
+    }
+    else
+    {
+        NSLog(@"Copy database to document successfully !");
+    }
+}
+
+#pragma mark **** SQL With Table Course ****
+
+- (void)deleteCourseWithCourse:(CourseModel *)course completion:(void (^)(BOOL success, NSString *message))callBack
+{
+    NSString *sql = [NSString stringWithFormat:@"DELETE FROM Course WHERE Course_ID = %d", [course courseId]];
+    
+    [self excuteSql:sql completion:^(BOOL success, NSString *message) {
+        callBack(success, message);
+    }];
+}
+
+- (void)updateCourseWithCourse:(CourseModel *)course completion:(void (^)(BOOL success, NSString *message))callBack
+{
+    NSString *sql = [NSString stringWithFormat:@"UPDATE Course SET Course_Name = '%@' WHERE Course_ID = %d", [course courseName], [course courseId]];
+    
+    [self excuteSql:sql completion:^(BOOL success, NSString *message) {
+        callBack(success, message);
+    }];
+}
+
+- (void)insertCourseWithName:(NSString *)courseName completion:(void (^)(BOOL success, NSString *message))callBack
+{
+    NSString *sql = [NSString stringWithFormat:@"INSERT INTO Course (Course_Name) VALUES ('%@')", courseName];
+    
+    [self excuteSql:sql completion:^(BOOL success, NSString *message) {
+        callBack(success, message);
+    }];
+}
+
+
+- (void)getAllCourseCompletion:(void (^)(BOOL success, NSArray *course,NSString *message ))callBack
+{
+    NSString *sql = @"SELECT Course_ID, Course_Name FROM Course";
+    
+    [self getDataWithSql:sql completion:^(BOOL success, sqlite3_stmt *sqlStatement, NSString *message) {
         
-        if (![context save:&error])
+        BOOL successFlag;
+        NSString *errorMessage;
+        NSMutableArray *courseList;
+        
+        if (success && sqlStatement != nil)
         {
-            return NO;
+            successFlag = YES;
+            errorMessage = nil;
+            
+            courseList = [[NSMutableArray alloc] init];
+            while (sqlite3_step(sqlStatement) == SQLITE_ROW)
+            {
+                CourseModel *course = [[CourseModel alloc] init];
+                course.courseId = sqlite3_column_int(sqlStatement, 0);
+                course.courseName = [NSString stringWithUTF8String:(char *)sqlite3_column_text(sqlStatement, 1)];
+                
+                [courseList addObject:course];
+            }
         }
         else
         {
-            return YES;
+            successFlag = NO;
+            errorMessage = message;
         }
-    }
-    else
-    {
-        return NO;
-    }
-}
-
-- (BOOL)deleteCourse:(Course *)course
-{
-    NSManagedObjectContext *context = [self getCurrentContext];
-    [context deleteObject:course];
-    
-    NSError *error = nil;
-    if (![context save:&error])
-    {
-        return NO;
-    }
-    else
-    {
-        return YES;
-    }
-}
-
-- (NSArray *)getAllCourse
-{
-    NSManagedObjectContext *context = [self getCurrentContext];
-    NSFetchRequest *request = [Course fetchRequest];
-    NSSortDescriptor *sort = [NSSortDescriptor sortDescriptorWithKey:@"courseName" ascending:YES];
-    request.sortDescriptors = @[sort];
-    NSError *error = nil;
-    
-    return [context executeFetchRequest:request error:&error];
-}
-
-#pragma mark - Table Student
-
--(BOOL)insertStudentWithName:(NSString *)studentName inCourse:(Course *)course
-{
-    NSManagedObjectContext *context = [self getCurrentContext];
-    
-    Student *st = [NSEntityDescription insertNewObjectForEntityForName:@"Student" inManagedObjectContext:context];
-    
-    st.studentName=studentName;
-    
-    [course addHasManyObject:st];
-    
-    NSError *error = nil;
-    
-    if(![context save:&error])
-    {
-        return NO;
-    }
-    else
-    {
-        return YES;
-    }
-}
-
-- (BOOL)editStudent:(Student *)student
-{
-    if (student != nil)
-    {
-        NSManagedObjectContext *context = [self getCurrentContext];
-        NSError *error = nil;
         
-        if (![context save:&error])
+        callBack(successFlag, courseList, errorMessage);
+    }];
+}
+
+#pragma mark **** SQL With Table Student ****
+
+- (void)deleteStudent:(StudentModel *)student completion:(void (^)(BOOL success, NSString *message))callBack
+{
+    NSString *sql = [NSString stringWithFormat:@"DELETE FROM Student WHERE Student_ID = %d", [student studentId]];
+    
+    [self excuteSql:sql completion:^(BOOL success, NSString *message) {
+        callBack(success, message);
+    }];
+}
+
+- (void)updateStudent:(StudentModel *)student completion:(void (^)(BOOL success, NSString *message))callBack
+{
+    NSString *sql = [NSString stringWithFormat:@"UPDATE Student SET Student_Name = '%@' WHERE Student_ID = %d", [student studentName], [student studentId]];
+    
+    [self excuteSql:sql completion:^(BOOL success, NSString *message) {
+        callBack(success, message);
+    }];
+}
+
+- (void)insertStudentWithName:(NSString *)studentName andCourse:(CourseModel *)course completion:(void (^)(BOOL success, NSString *message))callBack
+{
+    NSString *sql = [NSString stringWithFormat:@"INSERT INTO Student (Student_Name, Course_ID) VALUES ('%@',%d)", studentName, [course courseId]];
+    
+    [self excuteSql:sql completion:^(BOOL success, NSString *message) {
+        callBack(success, message);
+    }];
+}
+
+- (void)getAllStudentInCourse:(CourseModel *)course completion:(void (^)(BOOL success, NSArray *student, NSString *message ))callBack
+{
+    NSString *sql = [NSString stringWithFormat:@"SELECT Student_ID, Student_Name FROM Student WHERE Course_ID = %d", [course courseId]];;
+    
+    [self getDataWithSql:sql completion:^(BOOL success, sqlite3_stmt *sqlStatement, NSString *message) {
+        
+        BOOL successFlag;
+        NSString *errorMessage;
+        NSMutableArray *studentList;
+        
+        if (success && sqlStatement != nil)
         {
-            return NO;
+            successFlag = YES;
+            errorMessage = nil;
+            
+            studentList = [[NSMutableArray alloc] init];
+            while (sqlite3_step(sqlStatement) == SQLITE_ROW)
+            {
+                StudentModel *student = [[StudentModel alloc] init];
+                student.studentId = sqlite3_column_int(sqlStatement, 0);
+                student.studentName = [NSString stringWithUTF8String:(char *)sqlite3_column_text(sqlStatement, 1)];
+                student.courseId = sqlite3_column_int(sqlStatement, 2);
+                
+                [studentList addObject:student];
+            }
         }
         else
         {
-            return YES;
+            successFlag = NO;
+            errorMessage = message;
         }
-    }
-    else
-    {
-        return NO;
-    }
+        
+        callBack(successFlag, studentList, errorMessage);
+    }];
 }
 
-- (BOOL)deleteStudent:(Student *)student
+
+#pragma mark **** Base SQL ****
+
+- (void)excuteSql:(NSString *)sql completion:(void (^)(BOOL success, NSString *message))callBack
 {
-    NSManagedObjectContext *context = [self getCurrentContext];
-    [context deleteObject:student];
-    
-    NSError *error = nil;
-    if (![context save:&error])
-    {
-        return NO;
-    }
-    else
-    {
-        return YES;
-    }
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        
+        const char *databaseChar = [dbPath UTF8String];
+        BOOL success;
+        NSString *message;
+        
+        if (sqlite3_open(databaseChar, &db) == SQLITE_OK)
+        {
+            char *dbError;
+            if (sqlite3_exec(db, [sql UTF8String], NULL, NULL, &dbError) == SQLITE_OK)
+            {
+                success = YES;
+                message = [NSString stringWithFormat:@"Success to excute with sql: %@",sql];
+            }
+            else
+            {
+                success = NO;
+                message = [NSString stringWithFormat:@"Failed to excute with sql: %@",sql];
+            }
+        }
+        else
+        {
+            success = NO;
+            message = @"Can't open database";
+        }
+        sqlite3_close(db);
+        /// UI Thread
+        dispatch_async(dispatch_get_main_queue(), ^{
+            callBack(success, message);
+        });
+    });
 }
+
+- (void)getDataWithSql:(NSString *)sql completion:(void (^)(BOOL success, sqlite3_stmt *sqlStatement ,NSString *message ))callBack
+{
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        
+        const char *databaseChar = [dbPath UTF8String];
+        BOOL success;
+        NSString *message;
+        sqlite3_stmt *sqlStatement = nil;
+        
+        if (sqlite3_open(databaseChar, &db) == SQLITE_OK)
+        {
+            if (sqlite3_prepare(db, [sql UTF8String], -1, &sqlStatement, NULL) == SQLITE_OK)
+            {
+                if (sqlStatement != nil)
+                {
+                    success = YES;
+                    message = [NSString stringWithFormat:@"Success to get data with sql: %@", sql];
+                }
+                else
+                {
+                    success = NO;
+                    message = [NSString stringWithFormat:@"Failed to get data with sql: %@", sql];
+                }
+            }
+            else
+            {
+                success = NO;
+                message = [NSString stringWithFormat:@"Error to excute with sql: %@",sql];
+            }
+        }
+        else
+        {
+            success = NO;
+            message = @"Can't open database";
+        }
+        /// UI Thread
+        dispatch_async(dispatch_get_main_queue(), ^{
+            if (callBack != nil)
+            {
+                callBack(success, sqlStatement, message);
+            }
+            
+            if (sqlStatement != nil)
+            {
+                sqlite3_finalize(sqlStatement);
+            }
+            
+            sqlite3_close(db);
+            
+        });
+    });
+}
+
 
 @end
